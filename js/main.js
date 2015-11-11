7007eed45035e3edc22f3f6a1b238db1fd89bca3
lrfun <luorongfang126@126.com>
2015-11-11 14:08:10 +0800

    弹出窗

--- js/main.js
+++ js/main.js
@@ -14,4 +14,41 @@ $(document).ready(function () {
 		}
 	});
 	$(".backTop").click(function(){$("html,body").animate({scrollTop:"0px"},500);});
+
+	// 登陆和注册弹出窗
+	$('.dropBox').hide();
+	$('.regist').click(function(){
+		$('#login').fadeOut();			
+		$('#regist').fadeIn(500);
+	})
+	$('.login').click(function(){
+		$('#regist').fadeOut();			
+		$('#login').fadeIn(500);
+	})
+
+	//找回密码
+	$('.findPassword').click(function(){
+		$('#login').fadeOut();	
+		$('#findPassword').fadeIn(500);
+	})
+
+	//修改密码
+	$('.revisePassword').click(function(){
+		$('#findPassword').fadeOut();	
+		$('#revisePassword').fadeIn(500);
+	})
+
+	//关闭弹出穿
+	$('.dropBox').find('.closeBox').click(function(){
+		$('.dropBox').fadeOut(500);
+	})
+
+
+	//tab切换代码
+	$('.tab li').mouseover(function(){
+		var index=$(this).index();
+		$(this).addClass("cur").siblings('li').removeClass('cur');
+		$('.contentBox .tabList:eq('+ index +')').show().siblings().hide();
+	})
+
 })
